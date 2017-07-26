af227abe508640f861aa190f40aee517c6e1388c
Yves Schelpe <yves.schelpe@gmail.com>
2017-07-26 22:25:26 +0200

    styling and adding the field actions to the input element

--- ReduxSimpleStarter_router/src/components/posts/NewPost.js
+++ ReduxSimpleStarter_router/src/components/posts/NewPost.js
@@ -3,11 +3,47 @@ import { Field, reduxForm } from 'redux-form';
 
 export default reduxForm({ form: 'NewPostForm' })(
     class NewPost extends Component {
+        renderTitleField(field) {
+            return (
+                <div className="form-group">
+                    <label htmlFor="title">Title</label>
+                    <input
+                        {...field.input}
+                        id="title"
+                        className="form-control"
+                        type="text"
+                    />
+                </div>
+            );
+        }
+
+        renderCategoryField(field) {
+            return (
+                <div className="form-group">
+                    <label htmlFor="categories">Categories</label>
+                    <input
+                        {...field.input}
+                        id="categories"
+                        className="form-control"
+                        type="text"
+                    />
+                </div>
+            );
+        }
+
         render() {
             return (
                 <div>
                     <h3>Add new post</h3>
                     <form>
+                        <Field
+                            name="title"
+                            component={this.renderTitleField}
+                        />
+                        <Field
+                            name="categories"
+                            component={this.renderCategoryField}
+                        />
                     </form>
                 </div>
             );
