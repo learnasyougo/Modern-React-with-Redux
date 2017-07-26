f165c28990c5e9e0a3048a43d6f11c209e4808bf
Yves Schelpe <yves.schelpe@gmail.com>
2017-07-26 17:20:57 +0200

    initial reducer and initial state of posts recuder

--- ReduxSimpleStarter_router/src/reducers/index.js
+++ ReduxSimpleStarter_router/src/reducers/index.js
@@ -1,7 +1,8 @@
 import { combineReducers } from 'redux';
+import { PostsReducer } from './posts/PostsReducer';
 
 const rootReducer = combineReducers({
-  state: (state = {}) => state
+  posts: PostsReducer
 });
 
 export default rootReducer;
---
+++ ReduxSimpleStarter_router/src/reducers/posts/PostsReducer.js
@@ -0,0 +1,12 @@
+import {
+    FETCH_POSTS
+} from '../../actions/index';
+
+export default function (state = {}, action) {
+    switch (action.type) {
+        case FETCH_POSTS:
+            return state;
+        default:
+            return state;
+    }
+}
