e8056b112d23d204a13d2f5496ad2a0f01d9f949
Yves Schelpe <yves.schelpe@gmail.com>
2017-07-26 11:41:30 +0200

    Hooked up axios, react-redux and open weather forecast api

---
+++ ReduxSimpleStarter_weather/src/actions/index.js
@@ -0,0 +1,24 @@
+import promiseMiddleware from 'redux-promise';
+import axios from 'axios';
+
+const openWeatherMap = {
+    API: {
+        URL: 'http://api.openweathermap.org/data/2.5/forecast?q=',
+        KEY: 'bcc8fd3f8f43e1e6c55747689f03357b',
+        generate: (city, country) => `${openWeatherMap.API.URL}${city},${country}&appid=${openWeatherMap.API.KEY}`
+    }
+};
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city, country = 'BE') {
+    const requestUrl = openWeatherMap.API.generate(city, country);
+    console.info(`Built '${FETCH_WEATHER}' request URL '${requestUrl}'.`);
+
+    const request = axios.get(requestUrl);
+
+    return {
+        type: FETCH_WEATHER,
+        payload: request
+    };
+}
--- ReduxSimpleStarter_weather/src/containers/search-bar.js
+++ ReduxSimpleStarter_weather/src/containers/search-bar.js
@@ -1,45 +1,59 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { fetchWeather } from '../actions/index';
 
-export default class SearchBar extends Component {
-    state = {
-        searchTerm: ''
-    };
-
-    onInputChange(event) {
-        const searchTerm = event.target.value;
-        this.setState({
-            searchTerm
-        });
-    }
-
-    onSubmit(event) {
-        event.preventDefault();
-    }
-
-    render() {
-        return (
-            <form
-                onSubmit={e => this.onSubmit(e)}
-                className="input-group"
-            >
-
-                <input
-                    value={this.state.searchTerm}
-                    onChange={e => this.onInputChange(e)}
-                    className="form-control"
-                    placeholder="enter a city name"
-                />
-
-                <span className="input-group-btn">
-                    <button
-                        type="submit"
-                        className="btn btn-secondary"
-                    >
-                        Submit
-                    </button>
-                </span>
-
-            </form>
-        );
-    }
+function mapDispatchToProps(dispatch) {
+    return bindActionCreators({ fetchWeather }, dispatch);
 }
+
+export default connect(null, mapDispatchToProps)(
+    class SearchBar extends Component {
+        state = {
+            searchTerm: ''
+        };
+
+        onInputChange(event) {
+            const searchTerm = event.target.value;
+            this.setState({
+                searchTerm
+            });
+        }
+
+        onSubmit(event) {
+            event.preventDefault();
+            const searchTerm = this.state.searchTerm;
+            console.info(`Searching weather information for city '${searchTerm}'.`);
+            this.props.fetchWeather(searchTerm);
+            this.setState({ searchTerm: '' });
+        }
+
+        render() {
+            return (
+                <form
+                    onSubmit={e => this.onSubmit(e)}
+                    className="input-group"
+                >
+
+                    <input
+                        value={this.state.searchTerm}
+                        onChange={e => this.onInputChange(e)}
+                        className="form-control"
+                        placeholder="enter a city name"
+                    />
+
+                    <span className="input-group-btn">
+                        <button
+                            type="submit"
+                            className="btn btn-secondary"
+                        >
+                            Submit
+                        </button>
+                    </span>
+
+                </form>
+            );
+        }
+    }
+);
+
