5be01f345510400dce442cdeb933f7d69fa8ea11
Yves Schelpe <yves.schelpe@gmail.com>
2017-07-26 14:04:26 +0200

    styling changes svg

--- ReduxSimpleStarter_weather/src/components/weather-chart.js
+++ ReduxSimpleStarter_weather/src/components/weather-chart.js
@@ -11,16 +11,14 @@ export default props => {
     return (
         <div>
             <Sparklines
-                svgHeight={props.height || 100}
                 height={props.height || 100}
-                svgWidth={props.width || 150}
                 width={props.width || 150}
                 data={props.data}
             >
                 <SparklinesLine color={props.color || "grey"} />
                 <SparklinesBars color="grey" style={{ stroke: "white", fillOpacity: ".10" }} />
                 <SparklinesReferenceLine type="avg" />
-                <div><strong>Average: </strong>{average(props.data)} <i>{props.units}</i></div>
+                <div><strong>Average: </strong>{average(props.data)}<small><i>{props.units}</i></small></div>
             </Sparklines>
         </div>
     );
