9aeb79d3cc84b1102d4250c16db9ad7957f4155e
Yves Schelpe <yves.schelpe@gmail.com>
2017-07-26 10:46:25 +0200

    prevent default on form

--- ReduxSimpleStarter_weather/src/containers/search-bar.js
+++ ReduxSimpleStarter_weather/src/containers/search-bar.js
@@ -1,13 +1,44 @@
 import React, { Component } from 'react';
 
 export default class SearchBar extends Component {
+    state = {
+        searchTerm: ''
+    };
+
+    onInputChange(event) {
+        const searchTerm = event.target.value;
+        this.setState({
+            searchTerm
+        });
+    }
+
+    onSubmit(event) {
+        event.preventDefault();
+    }
+
     render() {
         return (
-            <form className="input-group">
-                <input />
+            <form
+                onSubmit={e => this.onSubmit(e)}
+                className="input-group"
+            >
+
+                <input
+                    value={this.state.searchTerm}
+                    onChange={e => this.onInputChange(e)}
+                    className="form-control"
+                    placeholder="enter a city name"
+                />
+
                 <span className="input-group-btn">
-                    <button type="submit" className="btn btn-secondary">Submit</button>
+                    <button
+                        type="submit"
+                        className="btn btn-secondary"
+                    >
+                        Submit
+                    </button>
                 </span>
+
             </form>
         );
     }
