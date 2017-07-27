88d9a91cd12662c8fa8ece43da9b188de6a801bc
Yves Schelpe <yves.schelpe@gmail.com>
2017-07-27 15:14:40 +0200

    delete post in stead in the recuder

--- ReduxSimpleStarter_router/src/reducers/posts/PostsReducer.js
+++ ReduxSimpleStarter_router/src/reducers/posts/PostsReducer.js
@@ -3,8 +3,7 @@ import _ from 'lodash';
 import {
     FETCH_POSTS,
     FETCH_POST,
-    DELETE_POST,
-    ADD_NEW_POST
+    DELETE_POST
 } from '../../actions/index'
 
 export default function (state = {}, action) {
@@ -14,6 +13,9 @@ export default function (state = {}, action) {
         case FETCH_POST:
             const post = action.payload.data;
             return { ...state, [post.id]: post };
+        case DELETE_POST:
+            const postId = action.payload;
+            return _.omit(state, postId);
         default:
             return state;
     }
